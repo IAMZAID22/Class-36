47e1edbb2c4b4e68ff071a3a2e536fa397bee9ea
IAMZAID22 <73696883+IAMZAID22@users.noreply.github.com>
2021-02-06 15:02:19 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,17 @@
+var gameState = 0,database,playerCount;
+var form,player,game
+
+function setup(){
+    database = firebase.database();
+    createCanvas(400,400);
+    
+    game = new Game ();
+    game.getState();
+    game.start();
+    
+    
+}
+
+function draw(){
+    
+}
